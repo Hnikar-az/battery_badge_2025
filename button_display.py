7cb6bfcea9b710fd955fec493368e129d278965c
hnikar <hnikar@hnikar.com>
2025-05-08 18:39:38 -0700

    software ready for deployment

--- button_display.py
+++ button_display.py
@@ -2,8 +2,9 @@ from machine import Pin, I2C
 # from setuptools.msvc import PlatformInfo
 from ssd1306 import SSD1306_I2C
 import time
+import os
 
-count = 0
+# count = 0
 plus_button = Pin(2, Pin.IN, Pin.PULL_UP) # needs a pin assignment (use the gpio pin numbers)
 minus_button = Pin(3, Pin.IN, Pin.PULL_UP)
 display_update_value = None
@@ -34,7 +35,24 @@ def display_update(count, display_update_value):
 
 def debouncer(wait_time):
     time.sleep(wait_time)
+def file_reader_or_creator(file):
+    print(file)
+    print(type(file))
+    if file in os.listdir():
+        print("file was there")
+        # file exists so can read
+        with open (file, "r") as file:
+            # files are stored as strings, convert to int
+            count = int(file.read())
+        return count
+    else:
+        # file does not exist
+        count = 0
+        return count
 
+
+count_storate_file = "count_storage.txt"
+count = file_reader_or_creator(count_storate_file)
 display_update_value = display_update(count, display_update_value) # to initialize the screen
 while True:
     if plus_button.value() == 0:
@@ -54,7 +72,7 @@ while True:
     else:
         button_press = False
     display_update_value = display_update(count, display_update_value)
-    print(count)
-
-
-
+    #print(count)
+    with open(count_storate_file, "w") as file:
+        # .write only accepts bytes or str
+        file.write(str(count))
